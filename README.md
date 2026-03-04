76ea826a0b847f1ceb3220b251b526234148cf69
Jan Ulrichts <jan.ulrichts@gmail.com>
2026-03-04 15:30:13 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prolog.NET
+
+A quick wrapper around the Prolog CLI using Proto.Actor for message-based communication.
---
+++ src/Prolog.NET.Actors/PrologActor.cs
@@ -0,0 +1,11 @@
+﻿using Proto;
+
+namespace Prolog.NET.Actors;
+
+public class PrologActor : IActor
+{
+    public Task ReceiveAsync(IContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
