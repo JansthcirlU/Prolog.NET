a5e9c7b4620503e3ef5b99c8ffaa6bed9e005b9a
Jan Ulrichts <jan.ulrichts@gmail.com>
2026-03-06 21:07:23 +0100

    Fixed DI method not being found.

--- src/Prolog.NET.Console/Program.cs
+++ src/Prolog.NET.Console/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Hosting;
 using Prolog.NET.Swipl;
 using Prolog.NET.Actors;
 using Prolog.NET.Console;
+using Microsoft.Extensions.DependencyInjection;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
