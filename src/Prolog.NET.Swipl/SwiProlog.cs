ad39f6d03370cc82ff72d00ce0e13f88a483eb07
jansthcirlu <jan.ulrichts@gmail.com>
2026-04-21 11:30:01 +0200

    Added initialise, is_initialised and halt to SwiProlog PInvoke class.

--- src/Prolog.NET.Swipl/SwiProlog.cs
+++ src/Prolog.NET.Swipl/SwiProlog.cs
@@ -1,6 +1,20 @@
-﻿namespace Prolog.NET.Swipl;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: InternalsVisibleTo("Prolog.NET.Swipl.Tests")]
+namespace Prolog.NET.Swipl;
 
 internal static unsafe partial class SwiProlog
 {
-    // Use [LibraryImport("swipl")]
+    [LibraryImport("swipl", EntryPoint = "PL_initialise")]
+    [return: MarshalAs(UnmanagedType.U1)]
+    internal static partial bool PL_initialise(int argc, char **argv);
+
+    [LibraryImport("swipl", EntryPoint = "PL_is_initialised")]
+    [return: MarshalAs(UnmanagedType.U1)]
+    internal static partial bool PL_is_initialised(int *argc, char ***argv);
+
+    [LibraryImport("swipl", EntryPoint = "PL_halt")]
+    [return: MarshalAs(UnmanagedType.U1)]
+    internal static partial bool PL_halt(int status);
 }
