c2afcdf6c4cedf6546481cb64d93c2c199de3763
jansthcirlu <jan.ulrichts@gmail.com>
2026-04-22 21:03:53 +0200

    Moved SwiProlog to C namespace with some useful enums. Test initialise and cleanup.

---
+++ src/Prolog.NET.Swipl/C/PL_CLEANUP_RESULT.cs
@@ -0,0 +1,9 @@
+namespace Prolog.NET.Swipl.C;
+
+internal enum PL_CLEANUP_RESULT
+{
+    PL_CLEANUP_RECURSIVE    = -2,
+    PL_CLEANUP_FAILED       = -1,
+    PL_CLEANUP_CANCELED     = 0,
+    PL_CLEANUP_SUCCESS      = 1
+}
---
+++ tests/Prolog.NET.Swipl.Tests/SwiPrologTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Prolog.NET.Swipl.C;
+
+namespace Prolog.NET.Swipl.Tests;
+
+public unsafe class SwiPrologTests
+{
+    [Fact]
+    public void PrologBasicLifecycleTest()
+    {
+        // Arrange
+        string firstArgument = OperatingSystem.IsLinux()
+            ? Environment.ProcessPath ?? "swipl"
+            : "swipl";
+        byte[] argumentBytes = Encoding.UTF8.GetBytes(firstArgument + "\0");
+        int argc = 1; // Argument count
+
+        // Act
+        bool initialised;
+        fixed (byte* argv0 = argumentBytes)
+        {
+            byte** argv = stackalloc byte*[argc]; // Arguments vector
+            argv[0] = argv0;
+            initialised = SwiProlog.PL_initialise(argc, argv);
+        }
+
+        int outArgc;
+        byte** outArgv;
+        bool confirm = SwiProlog.PL_is_initialised(&outArgc, &outArgv);
+        PL_CLEANUP_RESULT cleanup = SwiProlog.PL_cleanup(PL_CLEANUP_STATUS_AND_FLAGS.PL_CLEANUP_NO_CANCEL);
+
+        // Assert
+        Assert.True(initialised);
+        Assert.True(confirm);
+        Assert.Equal(PL_CLEANUP_RESULT.PL_CLEANUP_SUCCESS, cleanup);
+    }
+}
