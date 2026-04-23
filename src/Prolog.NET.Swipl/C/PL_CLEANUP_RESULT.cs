e31ec81454e7f35c9bde76a3f732a49837e4278a
jansthcirlu <jan.ulrichts@gmail.com>
2026-04-23 11:29:52 +0200

    Ran formatter.

--- src/Prolog.NET.Swipl/C/PL_CLEANUP_RESULT.cs
+++ src/Prolog.NET.Swipl/C/PL_CLEANUP_RESULT.cs
@@ -2,8 +2,8 @@ namespace Prolog.NET.Swipl.C;
 
 internal enum PL_CLEANUP_RESULT
 {
-    PL_CLEANUP_RECURSIVE    = -2,
-    PL_CLEANUP_FAILED       = -1,
-    PL_CLEANUP_CANCELED     = 0,
-    PL_CLEANUP_SUCCESS      = 1
+    PL_CLEANUP_RECURSIVE = -2,
+    PL_CLEANUP_FAILED = -1,
+    PL_CLEANUP_CANCELED = 0,
+    PL_CLEANUP_SUCCESS = 1
 }
