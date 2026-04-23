828cb94c76e2ffb8e6ecb17b2c61ec69d4fd7d20
jansthcirlu <jan.ulrichts@gmail.com>
2026-04-23 11:29:38 +0200

    Added test fixture and moved initialise check to test.

---
+++ tests/Prolog.NET.Swipl.Tests/SwiPrologTestFixture.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using System.Text;
+using Prolog.NET.Swipl.C;
+
+namespace Prolog.NET.Swipl.Tests;
+
+public class SwiPrologTestFixture : IAsyncLifetime
+{
+    public unsafe Task InitializeAsync()
+    {
+        // swipl check + Unix initialisation
+        string? swipl = Utils.Which("swipl").FirstOrDefault();
+        ArgumentNullException.ThrowIfNull(swipl);
+        if (OperatingSystem.IsLinux())
+        {
+            Environment.SetEnvironmentVariable("SWI_HOME_DIR", swipl);
+        }
+
+        // Initialise SWI-Prolog with quiet option
+        byte[] swiplArg = Encoding.UTF8.GetBytes("swipl\0");
+        byte[] quietArg = Encoding.UTF8.GetBytes("-q\0");
+        int argc = 2;
+        fixed (byte* argv0 = swiplArg)
+        fixed (byte* argv1 = quietArg)
+        {
+            byte** argv = stackalloc byte*[argc];
+            argv[0] = argv0;
+            argv[1] = argv1;
+            bool initialise = SwiProlog.PL_initialise(argc, argv);
+            Debug.Assert(initialise, "SWI-Prolog failed to initialise.");
+        }
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        PL_CLEANUP_RESULT cleanup = SwiProlog.PL_cleanup(PL_CLEANUP_STATUS_AND_FLAGS.PL_CLEANUP_NO_CANCEL);
+        Debug.Assert(cleanup == PL_CLEANUP_RESULT.PL_CLEANUP_SUCCESS, "SWI-Prolog cleanup was not successful.");
+        return Task.CompletedTask;
+    }
+}
