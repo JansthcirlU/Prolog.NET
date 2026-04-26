996e4ba2e0aa0415923c7b76038685ec67a8a427
jansthcirlu <jan.ulrichts@gmail.com>
2026-04-26 13:04:34 +0200

    Added NULL PL_engine_t to mean "no engine" when setting.

--- src/Prolog.NET.Swipl/C/PL_engine_t.cs
+++ src/Prolog.NET.Swipl/C/PL_engine_t.cs
@@ -5,6 +5,7 @@ namespace Prolog.NET.Swipl.C;
 [StructLayout(LayoutKind.Sequential)]
 internal struct PL_engine_t
 {
+    public static readonly PL_engine_t NULL = new() { handle = 0x0 };
     public static readonly PL_engine_t PL_ENGINE_MAIN = new() { handle = 0x1 };
     public static readonly PL_engine_t PL_ENGINE_CURRENT = new() { handle = 0x2 };
     public static readonly PL_engine_t PL_ENGINE_NONE = new() { handle = 0x3 };
