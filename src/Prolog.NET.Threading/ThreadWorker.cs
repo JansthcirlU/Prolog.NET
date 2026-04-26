30dfdf9be7cd65eb9912c9cd6a078c8a4a9c1665
jansthcirlu <jan.ulrichts@gmail.com>
2026-04-26 09:37:05 +0200

    Added quick worker type to run work on a separate thread. Added few sanity check tests.

---
+++ src/Prolog.NET.Threading/ThreadWorker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Concurrent;
+
+namespace Prolog.NET.Threading;
+
+/// <summary>
+/// Simple worker to allow running jobs on a separate thread.
+/// Assumes that any enqueued jobs succeeds without failure.
+/// Dispose finishes remaining work.
+/// </summary>
+/// <remarks>This type is not thread safe.</remarks>
+public class ThreadWorker : IDisposable
+{
+    private readonly Thread _workerThread;
+    private readonly BlockingCollection<Action> _workerQueue;
+
+    public ThreadWorker()
+    {
+        _workerQueue = [];
+        _workerThread = new(Run)
+        {
+            IsBackground = true
+        };
+        _workerThread.Start();
+    }
+
+    private void Run()
+    {
+        foreach (Action job in _workerQueue.GetConsumingEnumerable())
+        {
+            job.Invoke();
+        }
+    }
+
+    public void AddJob(Action job)
+    {
+        _workerQueue.Add(job);
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        _workerQueue.CompleteAdding();
+        _workerThread.Join();
+        _workerQueue.Dispose();
+    }
+}
---
+++ tests/Prolog.NET.Threading.Tests/SingleThreadWorkerTests.cs
@@ -0,0 +1,79 @@
+﻿namespace Prolog.NET.Threading.Tests;
+
+public class SingleThreadWorkerTests
+{
+    [Fact]
+    public void ThreadWorker_WhenDisposedWithEnqueuedJob_ShouldFinishJob()
+    {
+        // Arrange
+        ThreadWorker worker = new();
+
+        // Act
+        bool finished = false;
+        worker.AddJob(() => finished = true);
+        worker.Dispose();
+
+        // Assert
+        Assert.True(finished);
+    }
+
+    [Fact]
+    public void ThreadWorker_WhenDisposedWithMultipleEnqueuedJobs_ShouldFinishJobs()
+    {
+        // Arrange
+        ThreadWorker worker = new();
+
+        // Act
+        bool finished1 = false;
+        worker.AddJob(() => finished1 = true);
+        bool finished2 = false;
+        worker.AddJob(() => finished2 = true);
+        bool finished3 = false;
+        worker.AddJob(() => finished3 = true);
+        worker.Dispose();
+
+        // Assert
+        Assert.True(finished1);
+        Assert.True(finished2);
+        Assert.True(finished3);
+    }
+
+    [Fact]
+    public void ThreadWorker_WhenJobIsAdded_ShouldRunOnDifferentThread()
+    {
+        // Arrange
+        ThreadWorker worker = new();
+        int testThread = Environment.CurrentManagedThreadId;
+
+        // Act
+        int workerThread = -1;
+        worker.AddJob(() => workerThread = Environment.CurrentManagedThreadId);
+        worker.Dispose();
+
+        // Assert
+        Assert.NotEqual(-1, workerThread);
+        Assert.NotEqual(testThread, workerThread);
+    }
+
+    [Fact]
+    public void ThreadWorker_WhenMultipleJobsAreAdded_ShouldRunOnSameWorkerThread()
+    {
+        // Arrange
+        ThreadWorker worker = new();
+        int testThread = Environment.CurrentManagedThreadId;
+
+        // Act
+        int jobThread1 = -1;
+        worker.AddJob(() => jobThread1 = Environment.CurrentManagedThreadId);
+        int jobThread2 = -1;
+        worker.AddJob(() => jobThread2 = Environment.CurrentManagedThreadId);
+        worker.Dispose();
+
+        // Assert
+        Assert.NotEqual(-1, jobThread1);
+        Assert.NotEqual(testThread, jobThread1);
+        Assert.NotEqual(-1, jobThread2);
+        Assert.NotEqual(testThread, jobThread2);
+        Assert.Equal(jobThread1, jobThread2);
+    }
+}
